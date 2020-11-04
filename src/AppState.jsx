27a8ef347d5a8e1cdd17bb85500e89ecbf43c408
sali73 <sally.elgendy@hotmail.com>
2020-11-04 18:53:59 -0500

    create app state and route

---
+++ src/AppState.jsx
@@ -0,0 +1,34 @@
+import React, { useReducer } from 'react'
+
+///////////////////
+// initial state
+//////////////////
+const initialState = {
+    url: "http://ruby-on-rails-usernotes-app.herokuapp.com"
+}
+/////////////
+// readucer
+////////////
+const reducer= (state,action)=>{
+    switch(action.type){
+        default:
+            return state
+    }
+}
+/////////////////
+// appcontext
+////////////////
+const AppContext = React.createContext(null)
+/////////////////////
+//AppState Component
+/////////////////////
+export const AppState = (props)=>{
+    const [state,dispatch] = useReducer(reducer,initialState)
+    return <AppContext.Provider value={{state,dispatch}}>{props.children}</AppContext.Provider>
+}
+///////////////////////
+// useAppState hook
+//////////////////////
+export const useAppState=()=>{
+    return React.useContext(AppContext)
+}
