3804b6b52af7f5a6d82d0921b8d0706c912f390e
sali73 <sally.elgendy@hotmail.com>
2020-11-05 13:54:59 -0500

    create pages and nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,21 @@
 import React from "react";
+import {Switch, Route} from 'react-router-dom'
+import Nav from './Nav.jsx'
+import Home from '../pages/Home.jsx'
+import Auth from '../pages/Auth.jsx'
+import Dashboard from '../pages/Dashboard.jsx'
+
 
 export const App = (props) => {
-  return <h1>Hello World</h1>;
+  return <>
+  <Nav />
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route path='/auth/:form' component={Auth} />
+      <Route path='/dashboard' component={Dashboard} />
+    </Switch>
+  
+  
+  
+  </>;
 };
